3447f45cd0d25dcf57b67f14bca16199017010e0
Hannah Angle <hannahcangle@gmail.com>
2020-07-29 22:07:04 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
 def unsafe?(speed)
-
+  if speed > "60"
+    puts "Too fast"
+  elsif speed < "40"
+    puts "Too slow"
+  else 
+    puts "Good speed"
+  end
 end
 
-
-
-def not_safe?(speed)
-	
+def speed = 35
+speed < 40 ? "safe" : "not_safe?"
 end
-	
-
+def speed = 65
+  speed >  60 ? "safe" : "not_safe?"
+end
+def speed = 50
+  speed >= 60 ? "safe" : "not_safe?"
+end
 
